b662f12adc980bec96d89706ec8836a21e664ada
oskarito332 <gajos.oski@gmail.com>
2024-03-27 19:25:10 +0100

    fixed footer

--- apps/front/src/pages/layouts/MainLayout.jsx
+++ apps/front/src/pages/layouts/MainLayout.jsx
@@ -4,14 +4,15 @@ import styles from './styles.module.css';
 export default function MainLayout() {
     return (
         <>
-            <nav>
+
+            <header className={styles.header}>
+            <nav className={styles.nav}>
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/test">Test</a></li>
                     <li><a href="/login">login</a></li>
                 </ul>
             </nav>
-            <header>
                 <h1>Games page</h1>
             </header>
             <Outlet />
